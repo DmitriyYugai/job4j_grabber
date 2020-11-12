7427802199c73410c70e99a7e4817ad656c10181
Dmitriy Yugai <studentnstu97@mail.ru>
2020-11-12 13:56:31 +0700

    2. Парсинг HTML страницы. [#393952]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,8 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
 public class Main {
     public static int prnt(String s) {
         System.out.println(s);
@@ -6,5 +11,14 @@ public class Main {
 
     public static void main(String[] args) {
         prnt("Hello World");
+        LocalDateTime date = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofLocalizedDateTime(
+                FormatStyle.MEDIUM);
+        String text = date.format(formatter);
+        System.out.println(text);
+        String s = "24 нояб. 2020 г., 15:54:00";
+        LocalDateTime localDateTime = LocalDateTime.parse(s, formatter);
+        System.out.println(localDateTime);
+//        24 сен 20, 15:54
     }
 }

08296273e142f52aec70a01fe48aee55b96f42d2
Dmitriy Yugai <studentnstu97@mail.ru>
2020-11-13 12:58:04 +0700

    2.3. Загрузка деталей поста. [#393958]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -13,12 +13,38 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 import java.util.function.Supplier;
 
 public class SqlRuParse {
     private Map<String, Integer> months = new HashMap<>();
 
-    public Timestamp parseDate(String str) {
+    public Set<Post> parsePages() throws Exception {
+        for (int i = 1; i <= 5; i++) {
+            Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + i).get();
+            Elements row = doc.select(".postslisttopic");
+            for (Element td : row) {
+                Element href = td.child(0);
+                Elements siblings = td.nextElementSiblings();
+                Element date = siblings.get(3);
+                System.out.println(href.attr("href"));
+                System.out.println(href.text());
+                System.out.println(date.text());
+            }
+        }
+        return null;
+    }
+
+    public TwoTuple getDetail(String link) throws Exception {
+        Document doc = Jsoup.connect(link).get();
+        Element descElem = doc.select(".msgBody").get(1);
+        Element dateElem = doc.select(".msgFooter").get(0);
+        String desc = descElem.text();
+        String date = dateElem.text().substring(0, dateElem.text().indexOf('[') - 1);
+        return new TwoTuple(desc, parseDate(date));
+    }
+
+    private Timestamp parseDate(String str) {
         if (str.startsWith("сегодня")) {
             return getDate(str, LocalDate::now);
         } else if (str.startsWith("вчера")) {
@@ -58,22 +84,45 @@ public class SqlRuParse {
         months.put("дек", 12);
     }
 
-    public static void main(String[] args) throws Exception {
-        for (int i = 1; i <= 5; i++) {
-            Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + i).get();
-            Elements row = doc.select(".postslisttopic");
-            for (Element td : row) {
-                Element href = td.child(0);
-                Elements siblings = td.nextElementSiblings();
-                Element date = siblings.get(3);
-                System.out.println(href.attr("href"));
-                System.out.println(href.text());
-                System.out.println(date.text());
-            }
+    private static class TwoTuple {
+        private String desc;
+        private Timestamp created;
+
+        public TwoTuple(String desc, Timestamp created) {
+            this.desc = desc;
+            this.created = created;
         }
+
+        @Override
+        public String toString() {
+            return "TwoTuple{"
+                    + "desc='" + desc + '\''
+                    + ", created=" + created
+                    + '}';
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+//        for (int i = 1; i <= 5; i++) {
+//            Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + i).get();
+//            Elements row = doc.select(".postslisttopic");
+//            for (Element td : row) {
+//                Element href = td.child(0);
+//                Elements siblings = td.nextElementSiblings();
+//                Element date = siblings.get(3);
+//                System.out.println(href.attr("href"));
+//                System.out.println(href.text());
+//                System.out.println(date.text());
+//            }
+//        }
+
         SqlRuParse sqlRuParse = new SqlRuParse();
         System.out.println(sqlRuParse.parseDate("сегодня, 16:01"));
         System.out.println(sqlRuParse.parseDate("вчера, 22:01"));
         System.out.println(sqlRuParse.parseDate("6 ноя 20, 10:18"));
+
+        System.out.println(sqlRuParse.getDetail(
+                "https://www.sql.ru/forum/1330328/back-end-razrabotchik-pl-sql"));
+
     }
 }

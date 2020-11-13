9c261f961fcedcb744e60d5669cd650b8cc8e3fa
Dmitriy Yugai <studentnstu97@mail.ru>
2020-11-13 13:56:00 +0700

    2.4. SqlRuParse [#393959]

--- src/main/java/ru/job4j/html/Post.java
+++ src/main/java/ru/job4j/html/Post.java
@@ -4,14 +4,19 @@ import java.sql.Timestamp;
 import java.util.Objects;
 
 public class Post {
+    private long id;
+    private String name;
     private String desc;
+    private String link;
     private Timestamp crated;
 
     public Post() {
     }
 
-    public Post(String desc, Timestamp crated) {
+    public Post(String name, String desc, String link, Timestamp crated) {
+        this.name = name;
         this.desc = desc;
+        this.link = link;
         this.crated = crated;
     }
 
@@ -31,6 +36,30 @@ public class Post {
         this.crated = crated;
     }
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -40,12 +69,12 @@ public class Post {
             return false;
         }
         Post post = (Post) o;
-        return Objects.equals(desc, post.desc)
-                && Objects.equals(crated, post.crated);
+        return id == post.id
+                && Objects.equals(link, post.link);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(desc, crated);
+        return Objects.hash(id, link);
     }
 }
--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -5,65 +5,51 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.sql.Timestamp;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import java.time.format.FormatStyle;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-import java.util.function.Supplier;
+import java.util.ArrayList;
+import java.util.List;
 
-public class SqlRuParse {
+public class SqlRuParse implements Parse {
     private ParseDate pd;
 
     public SqlRuParse(ParseDate pd) {
         this.pd = pd;
     }
 
-    public Set<Post> parsePages() throws Exception {
-        for (int i = 1; i <= 5; i++) {
-            Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + i).get();
+    @Override
+    public List<Post> list(String link) {
+        List<Post> posts = new ArrayList<>();
+        try {
+            Document doc = Jsoup.connect(link).get();
             Elements row = doc.select(".postslisttopic");
-            for (Element td : row) {
-                Element href = td.child(0);
-                Elements siblings = td.nextElementSiblings();
-                Element date = siblings.get(3);
-                System.out.println(href.attr("href"));
-                System.out.println(href.text());
-                System.out.println(date.text());
+            for (int i = 3; i < row.size(); i++) {
+                Element href = row.get(i).child(0);
+                Post post = detail(href.attr("href"));
+                System.out.println("name: " + post.getName() + "link: "
+                        + post.getLink() + "created: " + post.getCrated() + System.lineSeparator());
+                posts.add(post);
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        return null;
+        return posts;
     }
 
-    private TwoTuple getDetail(String link) throws Exception {
-        Document doc = Jsoup.connect(link).get();
-        Element descElem = doc.select(".msgBody").get(1);
-        Element dateElem = doc.select(".msgFooter").get(0);
-        String desc = descElem.text();
-        String date = dateElem.text().substring(0, dateElem.text().indexOf('[') - 1);
-        return new TwoTuple(desc, pd.parseDate(date));
-    }
-
-    private static class TwoTuple {
-        private String desc;
-        private Timestamp created;
-
-        public TwoTuple(String desc, Timestamp created) {
-            this.desc = desc;
-            this.created = created;
+    @Override
+    public Post detail(String link) {
+        Post post = null;
+        try {
+            Document doc = Jsoup.connect(link).get();
+            Element descElem = doc.select(".msgBody").get(1);
+            Element dateElem = doc.select(".msgFooter").get(0);
+            Element nameElem = doc.select(".messageHeader").get(0);
+            String desc = descElem.text();
+            String date = dateElem.text().substring(0, dateElem.text().indexOf('[') - 1);
+            String name = nameElem.text().substring(0, nameElem.text().indexOf("[") - 1);
+            post = new Post(name, desc, link, pd.parseDate(date));
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
-        @Override
-        public String toString() {
-            return "TwoTuple{"
-                    + "desc='" + desc + '\''
-                    + ", created=" + created
-                    + '}';
-        }
+        return post;
     }
 
     public static void main(String[] args) throws Exception {
@@ -84,10 +70,16 @@ public class SqlRuParse {
         System.out.println(parseDate.parseDate("сегодня, 16:01"));
         System.out.println(parseDate.parseDate("вчера, 22:01"));
         System.out.println(parseDate.parseDate("6 ноя 20, 10:18"));
-
+//
         SqlRuParse sqlRuParse = new SqlRuParse(parseDate);
-        System.out.println(sqlRuParse.getDetail(
-                "https://www.sql.ru/forum/1330328/back-end-razrabotchik-pl-sql"));
+        Post post = sqlRuParse.detail(
+                "https://www.sql.ru/forum/1330254/java-razrabotchik-ee");
+        System.out.println(post.getName());
+        System.out.println(post.getLink());
+        System.out.println(post.getDesc());
+        System.out.println(post.getCrated());
+
+        sqlRuParse.list("https://www.sql.ru/forum/job-offers/13");
 
     }
 }

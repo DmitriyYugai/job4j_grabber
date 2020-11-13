1104583a9f4726afcabdd8451d88c6137125b9d2
Dmitriy Yugai <studentnstu97@mail.ru>
2020-11-13 16:52:43 +0700

    7. Web. [#393962]

--- src/main/java/ru/job4j/grabber/Grabber.java
+++ src/main/java/ru/job4j/grabber/Grabber.java
@@ -3,10 +3,9 @@ package ru.job4j.grabber;
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.*;
 
 import static org.quartz.JobBuilder.newJob;
@@ -51,6 +50,28 @@ public class Grabber implements Grab {
         scheduler.scheduleJob(job, trigger);
     }
 
+    public void web(Store store) {
+        new Thread(() -> {
+            try (ServerSocket server = new ServerSocket(
+                    Integer.parseInt(cfg.getProperty("port")))) {
+                while (!server.isClosed()) {
+                    Socket socket = server.accept();
+                    try (OutputStream out = socket.getOutputStream()) {
+                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                        for (Post post : store.getAll()) {
+                            out.write(post.toString().getBytes());
+                            out.write(System.lineSeparator().getBytes());
+                        }
+                    } catch (IOException io) {
+                        io.printStackTrace();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
     public static class GrabJob implements Job {
 
         @Override
@@ -63,7 +84,6 @@ public class Grabber implements Grab {
                 posts.addAll(parse.list("https://www.sql.ru/forum/job-offers/" + i));
             }
             for (Post post : posts) {
-                System.out.println("POSTING");
                 store.save(post);
             }
         }
@@ -75,7 +95,6 @@ public class Grabber implements Grab {
         Scheduler scheduler = grab.scheduler();
         Store store = grab.store();
         grab.init(new SqlRuParse(new ParseDate()), store, scheduler);
-        Thread.sleep(25000);
-        scheduler.shutdown();
+        grab.web(store);
     }
 }
